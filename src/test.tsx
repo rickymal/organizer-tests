7a320745f6ad0d91292f5863d322076668fbeab0
Henrique Mauler <henriquemauler@gmail.com>
2021-09-28 08:59:21 -0300

    changes

---
+++ src/test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Abc() {
+    return (
+        <div>
+            oi
+        </div>
+    )
+}
+
+function teste() {
+    return(
+        <div>
+            <Abc/>
+        </div>
+    )
+}
+
+export default teste
