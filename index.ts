fe6e85f96fa32a2be637d6253106e3a5a89b04a7
Henrique Mauler <henriquemauler@gmail.com>
2021-09-30 02:59:17 -0300

    changes

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import path from 'path'
-import teste from './src/test';
-import React from 'react'
-import ReactDOM from 'react-dom'
+
 const app = express()
 const PORT = 8000;
 const all_path = path.join(__dirname,'public')
 console.log(all_path)
+
 
 app.use(express.static(all_path))
 app.get('/',(req,res) => res.send("Express with typescript working"))
