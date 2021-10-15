98311ff2f0919ec5ac1347f6c9ff3806b6b515a5
Henrique Mauler <henriquemauler@gmail.com>
2021-10-15 12:20:08 -0300

    changes

--- index.ts
+++ index.ts
@@ -1,25 +1,42 @@
 import express from 'express'
 import path from 'path'
-
+import mongoose from 'mongoose';
 const app = express()
 const PORT = 8001;
 const all_path = path.join(__dirname,'public')
 console.log(all_path)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(all_path))
-// app.use(express.json())
+
+
+// Criar a instância do mongoose
+async function configMongo(){
+    await mongoose.connect('mongodb://localhost:27017/test');
+
+    const purpose_planning_schema = new mongoose.Schema({
+        project_name: String,
+        description : String,
+        task_duration : Date,
+        project_type : Number,
+      });
+      // continuar a partir da documentação
+      // https://mongoosejs.com/docs/
+
+      const task = mongoose.model("Purpose_Planning",)
+}
+
+
+
 app.get('/',(req,res) => res.send("Express with typescript working"))
 app.listen(PORT, () => {
     console.log("Servidor rodando")
 })
 
-
 app.post('/api/insert-task',(req,res) => {
     const data_query = req.query
-    const data_body = req.body
-    console.log (req)
+    console.log(req.body)
     res.send({
-        query: data_query,
-        body: data_body,
+        content : req.body,
     })
 })
-
